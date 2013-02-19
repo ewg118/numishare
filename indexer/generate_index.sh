d9ced3dc589ad406559c74bd9805b6118f31b3b5
Ethan Gruber <ewg4xuva@gmail.com>
2013-02-19 08:40:56 -0500

    fixed gitch in indexing script; xml declarations are suppressed from NUDS records in compilation.xml

--- indexer/generate_index.sh
+++ indexer/generate_index.sh
@@ -3,7 +3,7 @@ FILE=compilation.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $FILE
 echo '<content>' >> $FILE
 cat ../cocoon/xml/db/uva/config.xml >> $FILE
-for i in `ls ../cocoon/xml/db/uva/objects/*.xml`; do cat $i >> $FILE; done;
+for i in `ls ../cocoon/xml/db/uva/objects/*.xml`; do sed 's/<?xml\ version=\"1.0\"\ encoding=\"UTF-8\"?>//g' $i >> $FILE; done;
 echo '</content>' >> $FILE
 
 #process add doc
