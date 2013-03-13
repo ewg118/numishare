24bb2b7074ee6d659a4b1c1793114de035db2687
Wayne Graham <wayne.graham@virginia.edu>
2013-03-13 12:48:58 -0400

    Updtes the indexing script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-#set :stages, %w(production staging)
-#set :default_stage, "staging"
+set :stages, %w(production staging)
+set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
 set :application, "coins"
@@ -7,17 +7,27 @@ set :repository,  "git://github.com/ewg118/numishare.git"
 set :scn, :git
 set :branch, "uva"
 
+set :deploy_to, "/usr/local/projects/#{application}"
+set :deploy_via, :remote_cache
+
+set :normalize_asset_timestamps, false
+
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
+
+namespace :solr do
+  desc 'Runs the indexer/generate_index.sh script'
+  task :index, :roles => :app, :except => {:no_release => true} do
+    run "cd #{current_path}/indexer && ./generate_index.sh"
+  end
+end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart do; end
+end
