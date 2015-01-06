347520fd43e7fd63742bbd8964b8eff3edc1b3ae
Ethan Gruber <gruber@numismatics.org>
2015-01-06 11:50:10 -0500

    fixed typo in rdf

--- migration/berlin-lido-to-rdf.php
+++ migration/berlin-lido-to-rdf.php
@@ -69,14 +69,14 @@ function process_row($row, $count){
 				
 				//obverse
 				$rdf .= '<nm:obverse>';
-				$rdf .= '<foaf:thumbnail rdf:resource"http://ww2.smb.museum/mk_edit/images/' . $image_id . '/vs_thumb.jpg"/>';
-				$rdf .= '<foaf:depiction rdf:resource"http://ww2.smb.museum/mk_edit/images/' . $image_id . '/vs_opt.jpg"/>';
+				$rdf .= '<foaf:thumbnail rdf:resource="http://ww2.smb.museum/mk_edit/images/' . $image_id . '/vs_thumb.jpg"/>';
+				$rdf .= '<foaf:depiction rdf:resource="http://ww2.smb.museum/mk_edit/images/' . $image_id . '/vs_opt.jpg"/>';
 				$rdf .='</nm:obverse>';
 				
 				//reverse
 				$rdf .= '<nm:reverse>';
-				$rdf .= '<foaf:thumbnail rdf:resource"http://ww2.smb.museum/mk_edit/images/' . $image_id . '/rs_thumb.jpg"/>';
-				$rdf .= '<foaf:depiction rdf:resource"http://ww2.smb.museum/mk_edit/images/' . $image_id . '/rs_opt.jpg"/>';
+				$rdf .= '<foaf:thumbnail rdf:resource="http://ww2.smb.museum/mk_edit/images/' . $image_id . '/rs_thumb.jpg"/>';
+				$rdf .= '<foaf:depiction rdf:resource="http://ww2.smb.museum/mk_edit/images/' . $image_id . '/rs_opt.jpg"/>';
 				$rdf .='</nm:reverse>';
 			}
 			
