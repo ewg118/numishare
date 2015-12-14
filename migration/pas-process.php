fc5fc4df7d4a9762eb820b0356699a45aadc9d68
Ethan Gruber <ewg4xuva@gmail.com>
2015-12-14 15:27:13 -0500

    improved matching in the PAS processing PHP script

--- migration/pas-process.php
+++ migration/pas-process.php
@@ -7,11 +7,12 @@
  * See SPARQL query at https://gist.github.com/ewg118/9fbfc71772deb5e71964
  */
 
-$data = generate_json('pas_augustus.csv');
+$data = generate_json("queryResults.csv");
 $feed = 'http://numismatics.org/ocre/feed/';
 
 $results = array();
-$results[] = array('object', 'authority', 'mint', 'denomination', 'obvType', 'obvLegend', 'revType', 'revLegend', 'match1', 'score1', 'match2', 'score2', 'match3', 'score3', 'match4', 'score4', 'match5', 'score5');
+$results[] = array('object', 'authority', 'mint', 'denomination', 'obvType', 'obvLegend', 'revType', 'revLegend', 'altMint', 'altAuthority', 'match1', 'score1', 'match2', 'score2', 'match3', 'score3', 'match4', 'score4', 'match5', 'score5','match6','score6','match7','score7','match8','score8');
+
 foreach ($data as $row){
 	echo "Processing {$row['s']}\n";
 	
@@ -23,9 +24,19 @@ foreach ($data as $row){
 		$query['denomination_uri'] = trim($row['denomination']);
 	}
 	
+	/* Legend parsing rules:
+	 * 1. replace U with V
+	 * 2. replace line break forward slashes with whitespace
+	 * 3. strip left and right square brackets
+	 * 4. replace elipses with an asterisk for wildcard searches on incomplete legends
+	 * 5. do the same for multiple hyphens
+	 */
+	
 	//parse obverse legend
 	$obvLegend = trim(str_replace('U', 'V', str_replace('/', ' ', trim($row['obvLegend']))));
-	$obvLegend = str_replace('[', '', str_replace(']', '', trim($obvLegend)));
+	$obvLegend = str_replace('[', '', str_replace(']', '', preg_replace("/[\r|\n]/","",trim($obvLegend))));
+	$obvLegend = preg_replace('/\.+/', '*', $obvLegend);
+	$obvLegend = preg_replace('/[-]+/', '*', $obvLegend);
 	preg_match_all('/([A-Z]+)/', $obvLegend, $obvWords);
 	if (count($obvWords[0]) > 0){
 		$query['obv_leg_text'] = implode('+', $obvWords[0]);
@@ -33,11 +44,15 @@ foreach ($data as $row){
 	
 	//parse reverse legend
 	$revLegend = trim(str_replace('U', 'V', str_replace('/', ' ', trim($row['revLegend']))));
-	$revLegend = str_replace('[', '', str_replace(']', '', trim($revLegend)));
+	$revLegend = str_replace('[', '', str_replace(']', '', preg_replace("/[\r|\n]/","",trim($revLegend))));
+	$revLegend = preg_replace('/\.+/', '*', $revLegend);
+	$revLegend = preg_replace('/[-]+/', '*', $revLegend);
 	preg_match_all('/([A-Z]+)/', $revLegend, $revWords);
 	if (count($revWords[0]) > 0){
 		$query['rev_leg_text'] = implode('+', $revWords[0]);
 	}	
+	$altMint = '';
+	$altAuthority = '';
 	
 	//only execute query if there are obverse and reverse legends
 	$matches = array();
@@ -56,18 +71,70 @@ foreach ($data as $row){
 		$xpath->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
 		
 		$entries = $xpath->query("descendant::atom:entry");
-		$count = 0;
-		foreach ($entries as $entry){
-			if ($count < 5){
-				$id = $entry->getElementsByTagNameNS('http://www.w3.org/2005/Atom', 'id')->item(0)->nodeValue;
-				$score = $entry->getElementsByTagNameNS('http://a9.com/-/opensearch/extensions/relevance/1.0/', 'score')->item(0)->nodeValue;
-				$matches[$id] = $score;
+		$count = $entries->length;
+		
+		//if there is no response, then re-issue the query minus the mint
+		if ($count == 0){
+			$altMint = 'true';
+			unset($frags[0]);
+			$q = implode(' AND ', $frags);
+			$dom = new DOMDocument();
+			$dom->load("{$feed}?q={$q}&sort=score desc");
+			
+			$xpath = new DOMXpath($dom);
+			$xpath->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
+			
+			$entries = $xpath->query("descendant::atom:entry");
+			$count = $entries->length;
+			
+			
+			//if there are no matches, then remove the authority
+			if ($count == 0){
+				$altAuthority = 'true';
+				unset($frags[1]);
+				$q = implode(' AND ', $frags);
+				$dom = new DOMDocument();
+				$dom->load("{$feed}?q={$q}&sort=score desc");
+					
+				$xpath = new DOMXpath($dom);
+				$xpath->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
+					
+				$entries = $xpath->query("descendant::atom:entry");
+				$count = $entries->length;
+				if ($count > 0){
+					foreach ($entries as $entry){
+						$position = $entry->getNodePath();
+						if ($position <= 5){
+							$id = $entry->getElementsByTagNameNS('http://www.w3.org/2005/Atom', 'id')->item(0)->nodeValue;
+							$score = $entry->getElementsByTagNameNS('http://a9.com/-/opensearch/extensions/relevance/1.0/', 'score')->item(0)->nodeValue;
+							$matches[$id] = $score;
+						}
+					}
+				}
+			} else {
+				foreach ($entries as $entry){
+					$position = $entry->getNodePath();
+					if ($position <= 5){
+						$id = $entry->getElementsByTagNameNS('http://www.w3.org/2005/Atom', 'id')->item(0)->nodeValue;
+						$score = $entry->getElementsByTagNameNS('http://a9.com/-/opensearch/extensions/relevance/1.0/', 'score')->item(0)->nodeValue;
+						$matches[$id] = $score;
+					}
+				}
 			}
-			$count++;
+				
+		} else {
+			foreach ($entries as $entry){
+				$position = $entry->getNodePath();
+				if ($position <= 5){
+					$id = $entry->getElementsByTagNameNS('http://www.w3.org/2005/Atom', 'id')->item(0)->nodeValue;
+					$score = $entry->getElementsByTagNameNS('http://a9.com/-/opensearch/extensions/relevance/1.0/', 'score')->item(0)->nodeValue;
+					$matches[$id] = $score;					
+				}
+			}
 		}
 	}
 	
-	$record = array(trim($row['s']), trim($row['authority']), trim($row['mint']), trim($row['denomination']), trim($row['obvType']), trim($row['obvLegend']), trim($row['revType']), trim($row['revLegend']));
+	$record = array(trim($row['s']), trim($row['authority']), trim($row['mint']), trim($row['denomination']), preg_replace("/[\r|\n]/","",trim($row['obvType'])), preg_replace("/[\r|\n]/","",trim($row['obvLegend'])), preg_replace("/[\r|\n]/","",trim($row['revType'])), preg_replace("/[\r|\n]/","",trim($row['revLegend'])), $altMint, $altAuthority);
 	$count = 1;
 	foreach ($matches as $k=>$v){
 		$record[] = $k;
@@ -78,7 +145,7 @@ foreach ($data as $row){
 	$results[] = $record;
 }
 
-$fp = fopen('pas-concordances.csv', 'w');
+$fp = fopen("pas-concordances.csv", 'w');
 foreach ($results as $record) {
 	fputcsv($fp, $record);
 }
@@ -99,7 +166,7 @@ function generate_json($doc){
 	$labels = array_shift($csv);
 
 	foreach ($labels as $label) {
-		$keys[] = $label;
+		$keys[] = trim($label);
 	}
 
 	// Bring it all together
---
+++ migration/pas-reprocess.php
@@ -0,0 +1,59 @@
+<?php 
+
+$data = generate_json("pas-concordances.csv");
+
+$records = array();
+$records[] = array('object','match1','score1','match2','score2','match3','score3','match4','match4');
+foreach ($data as $row){
+	if (strlen($row['match2']) > 0 && strlen($row['match5']) == 0){
+		$records[] = array($row['object'], "http://numismatics.org/ocre/id/{$row['match1']}",$row['score1'], "http://numismatics.org/ocre/id/{$row['match2']}",$row['score2'], (strlen($row['match3']) > 0 ? "http://numismatics.org/ocre/id/{$row['match3']}" : ''),$row['score3'], (strlen($row['match4']) > 0 ? "http://numismatics.org/ocre/id/{$row['match4']}" : ''),$row['score4']);
+	}
+}
+
+$fp = fopen("pas-moderate.csv", 'w');
+foreach ($records as $record) {
+	fputcsv($fp, $record);
+}
+fclose($fp);
+
+//functions
+function generate_json($doc){
+	$keys = array();
+	$array = array();
+
+	$csv = csvToArray($doc, ',');
+
+	// Set number of elements (minus 1 because we shift off the first row)
+	$count = count($csv) - 1;
+
+	//Use first row for names
+	$labels = array_shift($csv);
+
+	foreach ($labels as $label) {
+		$keys[] = trim($label);
+	}
+
+	// Bring it all together
+	for ($j = 0; $j < $count; $j++) {
+		$d = array_combine($keys, $csv[$j]);
+		$array[$j] = $d;
+	}
+	return $array;
+}
+
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) {
+	if (($handle = fopen($file, 'r')) !== FALSE) {
+		$i = 0;
+		while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
+			for ($j = 0; $j < count($lineArray); $j++) {
+				$arr[$i][$j] = $lineArray[$j];
+			}
+			$i++;
+		}
+		fclose($handle);
+	}
+	return $arr;
+}
+
+?>
