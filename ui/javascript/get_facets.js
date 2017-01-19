228552c55bf007e0a5aca25ed6b9c36e3637d4c3
Ethan Gruber <ewg4xuva@gmail.com>
2017-01-19 14:34:02 -0500

    updated bootstrap multiselect version and enabled case insenstive search

--- ui/javascript/get_facets.js
+++ ui/javascript/get_facets.js
@@ -35,13 +35,13 @@ $(document).ready(function () {
 	//multiselect facets
 	$('.multiselect').multiselect({
 		buttonWidth: '250px',
-		enableFiltering: true,
+		enableCaseInsensitiveFiltering: true,
 		maxHeight: 250,
 		buttonText: function (options, select) {
 			if (options.length == 0) {
-				return select.attr('title') + ' <b class="caret"></b>';
+				return select.attr('title');
 			} else if (options.length > 2) {
-				return select.attr('title') + ': ' + options.length + ' selected <b class="caret"></b>';
+				return select.attr('title') + ': ' + options.length;
 			} else {
 				var selected = '';
 				options.each(function () {
@@ -51,7 +51,7 @@ $(document).ready(function () {
 				if (label.length > 20) {
 					label = label.substr(0, 20) + '...';
 				}
-				return select.attr('title') + ': ' + label + ' <b class="caret"></b>';
+				return select.attr('title') + ': ' + label;
 			}
 		},
 		onChange: function (element, checked) {
