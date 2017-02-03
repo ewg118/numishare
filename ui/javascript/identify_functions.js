63de0b3e3dfe2f53601f5518355b6a6838226ed6
Ethan Gruber <ewg4xuva@gmail.com>
2017-02-03 09:36:09 -0500

    syncing identify page JS and CSS to OCRE - is now fully functional

--- ui/javascript/identify_functions.js
+++ ui/javascript/identify_functions.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
 		$('#q_input').attr('value', query);
 	});
 	
-	
+	//on checking a material box, iterate through each portrait to show the coin of that metal, if avaiable
 	$('input[field=material]').click(function () {
 		if ($(this).is(':checked')) {
 			var material = $(this).attr('id').split('-')[1];
@@ -66,8 +66,10 @@ $(document).ready(function () {
 		}
 	});
 	
+	//pagination functions. display next or previous image
 	$('.page-next').click(function () {
-		var bg = $(this).closest('div.portrait').css('background-image').match(/url\(["|']?([^"']*)["|']?\)/)[1];
+		var bg = $(this).closest('div.portrait').children('.image-spacer').children('img').attr('src');
+		
 		var obj = jQuery.parseJSON($(this).closest('div.portrait').children('div[class=hidden]').text());
 		var list = new Array();
 		for (var material in obj) {
@@ -82,17 +84,9 @@ $(document).ready(function () {
 			if (list[i] == bg) {
 				//if the image is last
 				if ((i + 1) == list.length) {
-					$(this).closest('div.portrait').css({
-						'background': 'url(' + list[0] + ')',
-						'background-size': '100%',
-						'background-repeat': 'no-repeat'
-					});
+					$(this).closest('div.portrait').children('.image-spacer').children('img').attr('src', list[0]);
 				} else {					
-					$(this).closest('div.portrait').css({
-						'background': 'url(' + list[i+1] + ')',
-						'background-size': '100%',
-						'background-repeat': 'no-repeat'
-					});
+					$(this).closest('div.portrait').children('.image-spacer').children('img').attr('src', list[i+1]);
 				}
 			}
 		}
@@ -100,7 +94,7 @@ $(document).ready(function () {
 	});
 	
 	$('.page-prev').click(function () {
-		var bg = $(this).closest('div.portrait').css('background-image').match(/url\(["|']?([^"']*)["|']?\)/)[1];
+		var bg = $(this).closest('div.portrait').children('.image-spacer').children('img').attr('src');
 		var obj = jQuery.parseJSON($(this).closest('div.portrait').children('div[class=hidden]').text());
 		var list = new Array();
 		for (var material in obj) {
@@ -115,21 +109,23 @@ $(document).ready(function () {
 			if (list[i] == bg) {
 				//if the image is last
 				if (i == 0) {
-					$(this).closest('div.portrait').css({
-						'background': 'url(' + list[list.length - 1] + ')',
-						'background-size': '100%',
-						'background-repeat': 'no-repeat'
-					});
-				} else {					
-					$(this).closest('div.portrait').css({
-						'background': 'url(' + list[i-1] + ')',
-						'background-size': '100%',
-						'background-repeat': 'no-repeat'
-					});
+				    $(this).closest('div.portrait').children('.image-spacer').children('img').attr('src',list[list.length - 1]);
+				} else {	
+                    $(this).closest('div.portrait').children('.image-spacer').children('img').attr('src', list[i-1]);
 				}
 			}
 		}
 		return false;
+	});
+	
+	//check or uncheck the box if the image is clicked
+	$('.image-spacer').click(function(){
+	       var checkbox = $(this).next('.paginate-images').find('input[type=checkbox]');
+	       if (checkbox.is(':checked')) {
+	           checkbox.prop('checked', false);
+	       } else {
+	           checkbox.prop('checked', true);
+	       }
 	});
 });
 
@@ -138,11 +134,7 @@ function renderPortraits(material) {
 		var obj = jQuery.parseJSON($(this).children('div[class=hidden]').text());
 		if (material in obj) {
 			var url = obj[material][0];
-			$(this).css({
-				'background': 'url(' + url + ')',
-				'background-size': '100%',
-				'background-repeat': 'no-repeat'
-			});
+			$(this).children('.image-spacer').children('img').attr('src',url);			
 		}
 	});
 }
