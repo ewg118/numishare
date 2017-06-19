5baee3b45b0bd24d9dd5c0c06cb1f8f875f1c617
Ethan Gruber <ewg4xuva@gmail.com>
2017-06-19 12:42:59 -0400

    added 3D model viewing in JS

--- ui/javascript/display_functions.js
+++ ui/javascript/display_functions.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     $('a.thumbImage').fancybox({
         type: 'image',
         beforeShow: function () {
-            this.title = '<a href="' + this.element.attr('id') + '">' + this.element.attr('title') + '</a>'
+            this.title = '<a href="' + this.element.attr('id') + '">' + this.element.attr('title') + '</a>';
         },
         helpers: {
             title: {
@@ -35,6 +35,23 @@ $(document).ready(function () {
                 $(".iiif-container-template").clone().removeAttr('class').attr('id', 'iiif-container').appendTo("#iiif-window");
                 $('#manifest').text(manifest);
                 render_image(manifest);
+            }
+        },
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
+    
+    $('.model-button').fancybox({
+         beforeShow: function () {
+            var url = this.element.attr('model-url');
+            this.title = '<a href="' + this.element.attr('identifier') + '">' + this.element.attr('title') + '</a>';
+            //if the URL is sketchfab, then remove existing iframe and reload iframe
+            if (url.indexOf('sketchfab') > 0) {          
+                //$('#model-window').children('iframe').remove();
+                $("#model-iframe-template").clone().removeAttr('id').attr('src', url + '/embed').appendTo("#model-window");
             }
         },
         helpers: {
