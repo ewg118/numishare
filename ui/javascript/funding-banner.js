6cb8a5fd2a7187bb00ef5230309687fad380e2fe
Ethan Gruber <ewg4xuva@gmail.com>
2020-03-11 14:22:44 -0400

    updated wording of banner

--- ui/javascript/funding-banner.js
+++ ui/javascript/funding-banner.js
@@ -4,9 +4,10 @@ Function: A Wikidata style funding banner loaded in the config/includes*/
 $(document).ready(function () {
 
     var template = '<div id="funding"><div class="container">' + 
-    '<div class="row"><div class="col-md-12">In collaboration with Spink & Son, the American Numismatic Society ' + 
-    'is working to publish the new RIC Vol. II, Part 3 in OCRE. In order to help fund this labor, we asking for small donations. ' + 
-    'If you use OCRE for teaching or research, please consider <a href="https://charity.gofundme.com/o/en/campaign/updating-ocre" alt="Donate for OCRE">donating to the ANS.</a>' + 
+    '<div class="row"><div class="col-md-12">The new RIC Vol. II Part 3 just published by Spink & Son necessitates the replacement of about 4,000 '+ 
+    'OCRE records of Hadrian. The American Numismatic Society, OCRE editor, is asking for small donations to help completing that very ' + 
+    'significant improvement. If you use OCRE for teaching or research, please consider ' + 
+    '<a href="https://charity.gofundme.com/o/en/campaign/updating-ocre" alt="Donate for OCRE">donating to the ANS</a>. No amount is too small.' + 
     '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div></div></div>';
 
     if (!sessionStorage.alreadyClicked) {
