190ec46c6a1a107cd9ee575592e10d6f841ed814
Ethan Gruber <ewg4xuva@gmail.com>
2020-05-14 12:15:06 -0400

    tweak

--- ui/javascript/symbol_map_functions.js
+++ ui/javascript/symbol_map_functions.js
@@ -135,7 +135,7 @@ function initialize_map(uri) {
                 }
                 str += '<a href="' + feature.properties.gazetteer_uri + '">' + feature.properties.toponym + '</a></span>'
             }
-            if (feature.when.hasOwnProperty('closing_date') == true) {
+            if (feature.properties.hasOwnProperty('closing_date') == true) {
                 str += '<span>';
                 str += '<b>Closing Date: </b>' + feature.properties.closing_date;
             }
