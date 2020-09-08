2231718d857fc8c114c7492f1823e2c90d5026aa
Ethan Gruber <ewg4xuva@gmail.com>
2020-09-08 13:57:39 -0400

    deprecated TimeMap

--- ui/javascript/display_map_functions.js
+++ ui/javascript/display_map_functions.js
@@ -1,3 +1,8 @@
+/***
+ Author: Ethan Gruber
+ Date: September 2020
+ Function: Generate a Leaflet map for a specimen/type page and call the relevant GeoJSON serialization
+***/
 $(document).ready(function () {
     var id = $('title').attr('id');
     var collection_type = $('#collection_type').text();
@@ -5,66 +10,10 @@ $(document).ready(function () {
     var pipeline = $('#pipeline').text();
     var lang = $('#lang').text();
     
-    if (collection_type == 'hoard') {
-        if ($('#map').length > 0) {
-            initialize_timemap(id, path, lang);
-        }
-    } else if (collection_type == 'cointype') {
-        var hasFindspots = $('#hasFindspots').text();
-        
-        //display timemap only when there are findspots
-        if (hasFindspots == 'true') {
-            if ($('#map').length > 0) {
-                initialize_timemap(id, path, lang);
-            }
-        } else {
-            //otherwise display regular maps
-            if ($('#mapcontainer').length > 0) {
-                initialize_map(id, path, lang);
-            }
-        }
-    } else {
-        if ($('#mapcontainer').length > 0) {
-            initialize_map(id, path, lang);
-        }
+    if ($('#mapcontainer').length > 0) {
+        initialize_map(id, path, lang);
     }
 });
-
-function initialize_timemap(id, path, lang) {
-    var url = path + "apis/get?id=" + id + "&format=json&lang=" + lang;
-    var datasets = new Array();
-    
-    //first dataset
-    datasets.push({
-        id: 'dist',
-        title: "Distribution",
-        type: "json",
-        options: {
-            url: url
-        }
-    });
-    
-    var tm;
-    tm = TimeMap.init({
-        mapId: "map", // Id of map div element (required)
-        timelineId: "timeline", // Id of timeline div element (required)
-        options: {
-            mapType: "physical",
-            eventIconPath: $('#include_path').text() + "/images/timemap/"
-        },
-        datasets: datasets,
-        bandIntervals:[
-        Timeline.DateTime.YEAR,
-        Timeline.DateTime.DECADE]
-    });
-    function toggleDataset(dsid, toggle) {
-        if (toggle) {
-            tm.datasets[dsid].show();
-        } else {
-            tm.datasets[dsid].hide();
-        }
-    }
-}
 
 function initialize_map(id, path, lang) {
     var baselayers = $('#baselayers').text().split(',');
@@ -79,9 +28,9 @@ function initialize_map(id, path, lang) {
     });
     
     var imperium = L.tileLayer(
-    'http://dare.ht.lu.se/tiles/imperium/{z}/{x}/{y}.png', {
+    'https://dh.gu.se/tiles/imperium/{z}/{x}/{y}.png', {
         maxZoom: 10,
-        attribution: 'Powered by <a href="http://leafletjs.com/">Leaflet</a>. Map base: <a href="http://dare.ht.lu.se/" title="Digital Atlas of the Roman Empire, Department of Archaeology and Ancient History, Lund University, Sweden">DARE</a>, 2015 (cc-by-sa).'
+        attribution: 'Powered by <a href="http://leafletjs.com/">Leaflet</a>. Map base: <a href="https://dh.gu.se/dare/" title="Digital Atlas of the Roman Empire, Department of Archaeology and Ancient History, Lund University, Sweden">DARE</a>, 2015 (cc-by-sa).'
     });
     var mb_physical = L.tileLayer(
     'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxKey, {
@@ -99,7 +48,18 @@ function initialize_map(id, path, lang) {
     //add mintLayer from AJAX
     var overlay = L.geoJson.ajax(url, {
         onEachFeature: onEachFeature,
-        pointToLayer: renderPoints
+        style: function (feature) {
+            if (feature.geometry.type == 'Polygon') {
+                var fillColor = getFillColor(feature.properties.type);
+                
+                return {
+                    color: fillColor
+                }
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            return renderPoints(feature, latlng);
+        }
     }).addTo(map);
     
     //add controls
@@ -127,6 +87,8 @@ function initialize_map(id, path, lang) {
     //add controls
     var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
     
+    
+    
     //zoom to groups on AJAX complete
     overlay.on('data:loaded', function () {
         map.fitBounds(overlay.getBounds());
@@ -136,20 +98,8 @@ function initialize_map(id, path, lang) {
      * Features for manipulating layers
      *****/
     function renderPoints(feature, latlng) {
-        var fillColor;
-        switch (feature.properties.type) {
-            case 'mint':
-            fillColor = '#6992fd';
-            break;
-            case 'findspot':
-            fillColor = '#d86458';
-            break;
-            case 'subject':
-            fillColor = '#a1d490';
-            break;
-            default:
-            fillColor = '#efefef'
-        }
+        
+        var fillColor = getFillColor(feature.properties.type);
         
         return new L.CircleMarker(latlng, {
             radius: 5,
@@ -161,14 +111,50 @@ function initialize_map(id, path, lang) {
         });
     }
     
-    function onEachFeature (feature, layer) {
-        var label = feature.properties.name;
-        if (feature.properties.hasOwnProperty('uri')) {
-            str = label + ' <a href="' + feature.properties.uri + '" target="_blank"><span class="glyphicon glyphicon-new-window"/></a>';
-        } else {
-            str = label;
+    function getFillColor (type) {
+        var fillColor;
+        switch (type) {
+            case 'mint':
+            fillColor = '#6992fd';
+            break;
+            case 'findspot':
+            fillColor = '#d86458';
+            break;
+            case 'hoard':
+            fillColor = '#d86458';
+            break;
+            case 'subject':
+            fillColor = '#a1d490';
+            break;
+            default:
+            fillColor = '#efefef'
         }
         
+        return fillColor;
+    }
+    
+    function onEachFeature (feature, layer) {
+        var str;
+        //individual finds
+        if (feature.properties.hasOwnProperty('gazetteer_uri') == false) {
+            str = feature.label;
+        } else {
+            var str = '';
+            //display hoard link and gazetteer link
+            if (feature.hasOwnProperty('id') == true) {
+                str += '<a href="' + feature.id + '">' + feature.label + '</a><br/>';
+            }
+            if (feature.properties.hasOwnProperty('gazetteer_uri') == true) {
+                str += '<span>';
+                if (feature.properties.type == 'hoard') {
+                    str += '<b>Findspot: </b>';
+                }
+                str += '<a href="' + feature.properties.gazetteer_uri + '">' + feature.properties.toponym + '</a></span>';
+                if (feature.properties.type == 'hoard' && feature.properties.hasOwnProperty('closing_date') == true) {
+                    str += '<br/><b>Closing Date: </b>' + feature.properties.closing_date;
+                }
+            }
+        }
         layer.bindPopup(str);
     }
 }
