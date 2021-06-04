ab467905c300e8832f035db912fb449114f1caec
Ethan Gruber <ewg4xuva@gmail.com>
2021-06-04 16:47:30 -0400

    updated network functions

---
+++ ui/javascript/network_functions.js
@@ -0,0 +1,96 @@
+/*******
+FORCED NETWORK GRAPH FUNCTIONS FOR D3PLUS
+Modified: October 2020
+Function: These are the functions for generating a forced network graph for die and type pages when a die study is actived.
+ *******/
+$(document).ready(function () {
+    var collection_type = $('#collection_type').text();
+    var path = '../';
+    
+    /**** RENDER CHART ****/
+    $('.network-graph').each(function () {
+        var id = $(this).attr('id');
+        
+        var urlParams = {
+        };
+        
+        if (collection_type == 'cointype') {
+            urlParams[ 'type'] = $('#recordId').text();
+            urlParams[ 'namedGraph'] = $(this).attr('namedGraph');
+        } else if (collection_type == 'die') {
+            urlParams[ 'die'] = $('#recordId').text();
+            urlParams[ 'namedGraph'] = $(this).attr('namedGraph');
+        } else if (collection_type == 'symbol') {
+            urlParams[ 'uri'] = $('#objectURI').text();
+        }
+        
+        renderNetworkGraph(id, path, collection_type, urlParams);
+    });
+});
+
+//initiate a call to the getDieLinks JSON API and parse the resulting object into a d3plus Network
+function renderNetworkGraph(id, path, collection_type, urlParams) {
+    var api = (collection_type == 'symbol' ? 'getSymbolLinks': 'getDieLinks');
+    
+    //alert(urlParams);
+    $('#' + id).removeClass('hidden');
+    $('#' + id).height(600);
+    
+    $.get(path + 'apis/' + api, $.param(urlParams, true),
+    function (data) {
+        var nodeArray = data[ 'nodes'];
+        var edgeArray = data[ 'edges'];
+        
+        const network = new d3plus.Network().config({
+            links: edgeArray,
+            linkSize: function (edge) {
+                return edge.weight;
+            },
+            nodes: nodeArray,
+            groupBy: function (node) {
+                return node.side;
+            },
+            label: function (node) {
+                return node.label;
+            },
+            tooltipConfig: {
+                body: function (node) {
+                    if (node.hasOwnProperty('image')) {
+                        return '<div class="text-center"><img src="' + node.image + '" style="width:32px"/></div>';
+                    }
+                }
+            },
+            shapeConfig: {
+                backgroundImage: function (node) {
+                    if (node.hasOwnProperty('image')) {
+                        return node.image;
+                    }
+                },
+                label: false
+            },
+            color: function (node) {
+                switch (node.side){
+                    case 'obv':
+                        return '#282f6b';
+                        break;
+                    case 'rev':
+                        return '#b22200';
+                        break;
+                    case 'both':
+                        return '#7e12cc';
+                        break;
+                    case 'first':
+                        return '#6985c6';
+                        break;
+                    case 'second':
+                        return '#b3c9fc';
+                        break;
+                    default:
+                        return '#a8a8a8'
+                }
+            }
+        }).on("click", function (node) {
+            window.location.href = node.uri;
+        }).select('#' + id).render();
+    });
+}
