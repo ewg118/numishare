d661554fb0a02f765fd41cdc72a46679d1b9696d
Ethan Gruber <ewg4xuva@gmail.com>
2021-06-04 16:47:38 -0400

    updated network functions

--- ui/javascript/network_functions.js
+++ ui/javascript/network_functions.js
@@ -16,22 +16,27 @@ $(document).ready(function () {
         
         if (collection_type == 'cointype') {
             urlParams[ 'type'] = $('#recordId').text();
+            urlParams[ 'namedGraph'] = $(this).attr('namedGraph');
         } else if (collection_type == 'die') {
             urlParams[ 'die'] = $('#recordId').text();
+            urlParams[ 'namedGraph'] = $(this).attr('namedGraph');
+        } else if (collection_type == 'symbol') {
+            urlParams[ 'uri'] = $('#objectURI').text();
         }
-        urlParams[ 'namedGraph'] = $(this).attr('namedGraph');
         
-        renderNetworkGraph(id, path, urlParams);
+        renderNetworkGraph(id, path, collection_type, urlParams);
     });
 });
 
 //initiate a call to the getDieLinks JSON API and parse the resulting object into a d3plus Network
-function renderNetworkGraph(id, path, urlParams) {
+function renderNetworkGraph(id, path, collection_type, urlParams) {
+    var api = (collection_type == 'symbol' ? 'getSymbolLinks': 'getDieLinks');
+    
     //alert(urlParams);
     $('#' + id).removeClass('hidden');
     $('#' + id).height(600);
     
-    $.get(path + 'apis/getDieLinks', $.param(urlParams, true),
+    $.get(path + 'apis/' + api, $.param(urlParams, true),
     function (data) {
         var nodeArray = data[ 'nodes'];
         var edgeArray = data[ 'edges'];
@@ -48,15 +53,40 @@ function renderNetworkGraph(id, path, urlParams) {
             label: function (node) {
                 return node.label;
             },
+            tooltipConfig: {
+                body: function (node) {
+                    if (node.hasOwnProperty('image')) {
+                        return '<div class="text-center"><img src="' + node.image + '" style="width:32px"/></div>';
+                    }
+                }
+            },
+            shapeConfig: {
+                backgroundImage: function (node) {
+                    if (node.hasOwnProperty('image')) {
+                        return node.image;
+                    }
+                },
+                label: false
+            },
             color: function (node) {
-                if (node.side == 'obv') {
-                    return '#282f6b'
-                } else if (node.side == 'rev') {
-                    return '#b22200';
-                } else if (node.side == 'both') {
-                    return '#7e12cc';
-                } else {
-                    return '#a8a8a8';
+                switch (node.side){
+                    case 'obv':
+                        return '#282f6b';
+                        break;
+                    case 'rev':
+                        return '#b22200';
+                        break;
+                    case 'both':
+                        return '#7e12cc';
+                        break;
+                    case 'first':
+                        return '#6985c6';
+                        break;
+                    case 'second':
+                        return '#b3c9fc';
+                        break;
+                    default:
+                        return '#a8a8a8'
                 }
             }
         }).on("click", function (node) {
