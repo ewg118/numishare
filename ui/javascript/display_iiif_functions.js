f761fdcd626f5864ee6e9ff4eadd4ce2dcacb900
Ethan Gruber <ewg4xuva@gmail.com>
2022-07-25 12:26:03 -0400

    generalizing display of IIIF images for an object so that more than two can be accommodated

--- ui/javascript/display_iiif_functions.js
+++ ui/javascript/display_iiif_functions.js
@@ -4,25 +4,18 @@ Written by Ethan Gruber, gruber@numismatics.org
 Library: jQuery
 Description: Render IIIF images for physical coin records
  ************************************/
-$(document).ready(function () {
+$(document).ready(function () {    
     
-    //add obverse IIIF image
-    var obvInfo = $('#obv-iiif-service').text() + '/info.json';
-    var obverseIIIF = L.map('obv-iiif-container', {
-        center:[0, 0],
-        crs: L.CRS.Simple,
-        zoom: 0
+    $('.iiif-container').each(function () {
+        var service = $(this).attr('service') + '/info.json';
+        var id = $(this).attr('id');
+        
+        var leaflet = L.map(id, {
+            center:[0, 0],
+            crs: L.CRS.Simple,
+            zoom: 0
+        });
+        
+        L.tileLayer.iiif(service).addTo(leaflet);
     });
-    
-    L.tileLayer.iiif(obvInfo).addTo(obverseIIIF);    
-    
-     //add reverse IIIF image
-      var revInfo = $('#rev-iiif-service').text() + '/info.json';
-      var reverseIIIF = L.map('rev-iiif-container', {
-        center:[0, 0],
-        crs: L.CRS.Simple,
-        zoom: 0
-    });
-    
-    L.tileLayer.iiif(revInfo).addTo(reverseIIIF); 
 });
