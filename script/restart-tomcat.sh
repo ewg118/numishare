1bc44e6e0fd8c1f24518ac3ba7e4552918cb85bc
Ethan Gruber <ewg4xuva@gmail.com>
2025-01-08 10:31:14 -0500

    added hook to restart apache if tomcat is up

--- script/restart-tomcat.sh
+++ script/restart-tomcat.sh
@@ -10,7 +10,20 @@ while [ $retry_count -lt $max_retries ]; do
   curl -m 10 http://localhost:8080/orbeon/numishare/
 
   if [ $? -eq 0 ]; then
-    echo "Request was successful"
+    echo "Tomcat request was successful"
+    
+    #if Tomcat is successful, try Apache
+    curl -m 10 http://localhost/
+    if [ $? -eq 0 ]; then
+        echo "Apache request was successful"
+        break
+    else
+        echo "Restarting Apache"
+        service apache2 stop
+        service apache2 start
+    fi
+    
+    
     break
   else
     echo "Request failed with exit code $?. Retrying..."
