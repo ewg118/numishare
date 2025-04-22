ea2a46e45bfb1f46a6711d24475c42809fbd16b5
Ethan Gruber <ewg4xuva@gmail.com>
2025-04-22 10:52:08 -0400

    merged updated search functions from master

--- ui/javascript/search.js
+++ ui/javascript/search.js
@@ -1,14 +1,35 @@
 /************************************
-SEARCH
-Written by Ethan Gruber, gruber@numismatics.org
-Library: jQuery
-Description: Portions of this originally authored by Matt Mitchell.
-Modified heavily to handle the search form functionality
-and piecing together the search query.
-************************************/
-$(document).ready(function() {	
-	$('#advancedSearchForm').submit(function() {
-		var q = assembleQuery('advancedSearchForm');
-		$('#q_input') .attr('value', q);
-	});
+ SEARCH
+ Written by Ethan Gruber, egruber@numismatics.org
+ Date Modified: April 2025
+ Library: jQuery
+ Description: 2025 rewrite of Advanced Search for for Numishare
+ ************************************/
+$(document).ready(function () {
+    
+    //assemble query on form submission
+    $('#facet_form').submit(function () {
+        var q = getQuery();
+        $('#facet_form_query').attr('value', q);
+    });
+    
+    $('.addBtn').click(function () {
+        var id = $(this).attr('id').replace('add-', '');
+        
+        //alert(id);
+        
+        var template = $('#' + id + '-search').clone().removeAttr('id');
+        template.children('.removeBtn').removeClass('hidden');
+        
+        $('#' + id + '-container').append(template);
+    });
+    
+    $('.section-container').on('click', '.form-group .removeBtn', function () {
+        
+        // fade out the entire template
+        $(this).parent().fadeOut('fast', function () {
+            $(this).remove();
+        });
+        return false;
+    });
 });
