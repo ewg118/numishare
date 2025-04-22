ba9844b643b43da0ccef2bb8ddfd038f72d33001
Ethan Gruber <ewg4xuva@gmail.com>
2025-04-22 11:55:13 -0400

    merged updated search functions from master

--- ui/javascript/result_functions.js
+++ ui/javascript/result_functions.js
@@ -1,110 +1,116 @@
 /* generic result page functions:
-combines hard-coded fancybox call from XSLT, condenses sort_results and quick_search JS into single file
-*/
+ combines hard-coded fancybox call from XSLT, condenses sort_results and quick_search JS into single file
+ */
 $(document).ready(function () {
-	$('a.thumbImage').fancybox({
-		type: 'image',
-		beforeShow: function () {
-			this.title = '<a href="' + this.element.attr('id') + '">' + this.element.attr('title') + '</a>'
-		},
-		helpers: {
-			title: {
-				type: 'inside'
-			}
-		}
-	});
-	
-	//toggle divs
-	$('.toggle-button').click(function(){
-	           var id = '#' + $(this).attr('id').split('-')[1] + '-container';
-		if ($(this).children('span').hasClass('glyphicon-triangle-bottom')) {
-			$(this).children('span').removeClass('glyphicon-triangle-bottom');
-			$(this).children('span').addClass('glyphicon-triangle-right');
-		} else if ($(this).children('span').hasClass('glyphicon-triangle-right')) {
-			$(this).children('span').removeClass('glyphicon-triangle-right');
-			$(this).children('span').addClass('glyphicon-triangle-bottom');
-		} else if ($(this).children('span').hasClass('glyphicon-plus')) {
-			$(this).children('span').removeClass('glyphicon-plus');
-			$(this).children('span').addClass('glyphicon-minus');
-		} else if ($(this).children('span').hasClass('glyphicon-minus')) {
-			$(this).children('span').removeClass('glyphicon-minus');
-			$(this).children('span').addClass('glyphicon-plus');
-		}
-		$(id).toggle('fast');
-		return false;		
-	});
-	
-	$('.sortForm_categories') .change(function () {
-		var field = $(this).val();
-		var sort_order = $('.sortForm_order').val();
-		setValue(field, sort_order);
-	});
-	$('.sortForm_order') .change(function () {
-		var field = $('.sortForm_categories').val();
-		var sort_order = $(this).val();
-		setValue(field, sort_order);
-	});
-	
-	function setValue(field, sort_order) {
-		var category;
-		if (field.indexOf('_') > 0 || field == 'timestamp' || field == 'recordId') {
-			category = field;
-		} else {
-			if (sort_order == 'asc') {
-				switch (field) {
-					case 'year':
-					category = field + '_minint';
-					break;
-					case 'axis':
-					case 'diameter':
-					case 'taq':
-					case 'tpq':					
-					case 'weight':
-					category = field + '_num';
-					break;
-					default:
-					category = field + '_min';
-				}
-			} else if (sort_order == 'desc') {
-				switch (field) {
-					case 'year':
-					category = field + '_maxint';
-					break;
-					case 'axis':
-					case 'diameter':
-					case 'taq':
-					case 'tpq':					
-					case 'weight':
-					category = field + '_num';
-					break;
-					default:
-					category = field + '_max';
-				}
-			}
-		}
-		if (field != 'null') {
-			$('.sort_button') .prop('disabled', false);
-			$('.sort_param') .attr('value', category + ' ' + sort_order);
-		} else {
-			$('.sort_button') .prop('disabled', true);
-		}
-	}
-	
-	$('#qs_form').submit(function () {
-		assembleQuery();
-	});
-	
-	function assembleQuery() {
-		var search_text = $('#qs_text') .val();
-		var query = $('#qs_query').val();
-		if (search_text != null && search_text != '') {
-			if (query == '*:*' || query.length == 0) {
-				$('#qs_query') .attr('value', 'fulltext:' + search_text);
-			} else {
-				$('#qs_query') .attr('value', query + ' AND fulltext:' + search_text);
-			}
-		} else {
-			$('#qs_query') .attr('value', '*:*');
-		}
-	}
+    $('a.thumbImage').fancybox({
+        type: 'image',
+        beforeShow: function () {
+            this.title = '<a href="' + this.element.attr('id') + '">' + this.element.attr('title') + '</a>'
+        },
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+    });
+    
+    //toggle divs
+    $('.toggle-button').click(function () {
+        var id = '#' + $(this).attr('id').split('-')[1] + '-container';
+        if ($(this).children('span').hasClass('glyphicon-triangle-bottom')) {
+            $(this).children('span').removeClass('glyphicon-triangle-bottom');
+            $(this).children('span').addClass('glyphicon-triangle-right');
+        } else if ($(this).children('span').hasClass('glyphicon-triangle-right')) {
+            $(this).children('span').removeClass('glyphicon-triangle-right');
+            $(this).children('span').addClass('glyphicon-triangle-bottom');
+        } else if ($(this).children('span').hasClass('glyphicon-plus')) {
+            $(this).children('span').removeClass('glyphicon-plus');
+            $(this).children('span').addClass('glyphicon-minus');
+        } else if ($(this).children('span').hasClass('glyphicon-minus')) {
+            $(this).children('span').removeClass('glyphicon-minus');
+            $(this).children('span').addClass('glyphicon-plus');
+        }
+        $(id).toggle('fast');
+        return false;
+    });
+    
+    $('.sortForm_categories').change(function () {
+        var field = $(this).val();
+        var sort_order = $('.sortForm_order').val();
+        setValue(field, sort_order);
+    });
+    $('.sortForm_order').change(function () {
+        var field = $('.sortForm_categories').val();
+        var sort_order = $(this).val();
+        setValue(field, sort_order);
+    });
+    
+    function setValue(field, sort_order) {
+        var category;
+        if (field.indexOf('_') > 0 || field == 'timestamp' || field == 'recordId') {
+            category = field;
+        } else {
+            if (sort_order == 'asc') {
+                switch (field) {
+                    case 'year':
+                    category = field + '_minint';
+                    break;
+                    case 'axis':
+                    case 'diameter':
+                    case 'taq':
+                    case 'tpq':
+                    case 'weight':
+                    category = field + '_num';
+                    break;
+                    default:
+                    category = field + '_min';
+                }
+            } else if (sort_order == 'desc') {
+                switch (field) {
+                    case 'year':
+                    category = field + '_maxint';
+                    break;
+                    case 'axis':
+                    case 'diameter':
+                    case 'taq':
+                    case 'tpq':
+                    case 'weight':
+                    category = field + '_num';
+                    break;
+                    default:
+                    category = field + '_max';
+                }
+            }
+        }
+        if (field != 'null') {
+            $('.sort_button').prop('disabled', false);
+            $('.sort_param').attr('value', category + ' ' + sort_order);
+        } else {
+            $('.sort_button').prop('disabled', true);
+        }
+    }
+    
+    //assemble query on form submission
+    $('#facet_form').submit(function () {
+        var q = getQuery();
+        $('#facet_form_query').attr('value', q);
+    });
+    
+    $('#qs_form').submit(function () {
+        assembleQuery();
+    });
+    
+    function assembleQuery() {
+        var search_text = $('#qs_text').val();
+        var query = $('#qs_query').val();
+        if (search_text != null && search_text != '') {
+            if (query == '*:*' || query.length == 0) {
+                $('#qs_query').attr('value', 'fulltext:' + search_text);
+            } else {
+                $('#qs_query').attr('value', query + ' AND fulltext:' + search_text);
+            }
+        } else {
+            $('#qs_query').attr('value', '*:*');
+        }
+    }
 });
