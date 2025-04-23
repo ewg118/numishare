af3cf4e52a805ca9845c0a62f738af127bdcad68
Ethan Gruber <ewg4xuva@gmail.com>
2025-04-23 14:15:31 -0400

    added apache restart to tomcat restart function

--- script/restart-tomcat.sh
+++ script/restart-tomcat.sh
@@ -3,13 +3,10 @@
 #restarts tomcat after a timeout. the script should be run in cron as root
 
 service=tomcat10
-max_retries=3
-retry_count=0
 
-while [ $retry_count -lt $max_retries ]; do
-  curl -m 10 http://localhost:8080/orbeon/numishare/
+curl -m 10 http://localhost:8080/orbeon/numishare/
 
-  if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "Tomcat request was successful"
     
     #if Tomcat is successful, try Apache
@@ -22,20 +19,14 @@ while [ $retry_count -lt $max_retries ]; do
         echo $(date --utc +%FT%T.%3NZ) "Restarted Apache" >> /var/log/$service/restart.log
         service apache2 stop
         service apache2 start
-    fi
-    
+    fi    
     
     break
-  else
-    echo "Request failed with exit code $?. Retrying..."
-    retry_count=$((retry_count + 1))
-    sleep 2 # Wait for 2 seconds before retrying
-  fi
-done
-
-if [ $retry_count -eq $max_retries ]; then
-  echo "Restarting Tomcat"
-  echo $(date --utc +%FT%T.%3NZ) "Restarted Tomcat" >> /var/log/$service/restart.log
-  service $service stop
-  service $service start
+else
+    echo "Restarting Apache"
+    service apache2 restart
+    echo "Restarting Tomcat"
+    echo $(date --utc +%FT%T.%3NZ) "Restarted Tomcat" >> /var/log/$service/restart.log
+    service $service stop
+    service $service start
 fi
