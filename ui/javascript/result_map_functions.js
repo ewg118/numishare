4e0ae0051686b25006b069b26f6f4d4009bc9b39
Ethan Gruber <ewg4xuva@gmail.com>
2025-05-19 14:03:58 -0400

    the resultMap now toggle in the div rather than popup

--- ui/javascript/result_map_functions.js
+++ ui/javascript/result_map_functions.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
     var q = $('#current-query').text();
-    $("#map_results").fancybox({
-        beforeShow: function () {
-            if ($('#resultMap').html().length == 0) {
-                $('#resultMap').html('');
-                initialize_map(q);
-            }
+    
+    
+    $("#map_results").click(function () {
+        $('#resultMap').toggle();
+        //only load map if the div is empty
+        if ($('#resultMap').html().length == 0) {
+            $('#resultMap').html('')            
+            initialize_map(q);
         }
+        
+        if ($(this).text() == 'View Map') {
+            $(this).text('Hide Map');
+        } else {
+            $(this).text('View Map'); 
+        }
+        
+        return false;
     });
     
     function initialize_map(q) {
@@ -133,6 +143,7 @@ $(document).ready(function () {
             var fillColor;
             switch (feature.properties.type) {
                 case 'mint':
+                case 'productionPlace':
                 fillColor = '#6992fd';
                 break;
                 case 'hoard':
